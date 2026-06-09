d2fc68d3c448ae995c5d461a6afb7f871ac2c874
Local Developer <local@developer.com>
2026-06-09 14:14:49 +0400

    extend vs implement thread / runnable

---
+++ multithreading/src/main/java/threadvsrunnable/A.java
@@ -0,0 +1,11 @@
+package threadvsrunnable;
+
+public class A extends B implements Runnable{
+    /*
+        **USE IMPLEMENT RUNNABLE** WHEN ALREADY EXTENDING A CLASS
+    */
+    @Override
+    public void run() {
+
+    }
+}
