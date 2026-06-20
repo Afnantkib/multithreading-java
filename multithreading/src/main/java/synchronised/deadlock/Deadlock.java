fa45e3a86e2f32960252d90461a07a3b3d6ac43b
Mohammad Afnan <83164779+Afnantkib@users.noreply.github.com>
2026-06-20 14:12:38 +0400

    deadlock with example

--- multithreading/src/main/java/synchronised/deadlock/Deadlock.java
+++ multithreading/src/main/java/synchronised/deadlock/Deadlock.java
@@ -6,30 +6,55 @@ public class Deadlock {
   Object lockB = new Object();
 
   void usePen() throws InterruptedException {
-    Thread.sleep(50);
-    System.out.println(Thread.currentThread().getName() + " trying to acquire lock on LOCK B");
+    Thread.sleep(1000);
+    System.out.println(Thread.currentThread().getName() + " trying to acquire lock on LOCK A");
     synchronized (lockA) {
       System.out.println(Thread.currentThread().getName() + " acquired lock on LOCK A");
+      System.out.println(Thread.currentThread().getName() + " trying to acquire lock on LOCK B");
+
+      synchronized (lockB) {
+        System.out.println(Thread.currentThread().getName() + " acquired lock on LOCK B");
+      }
     }
-    usePaper();
   }
 
   void usePaper() throws InterruptedException {
-    Thread.sleep(50);
+    Thread.sleep(1000);
     System.out.println(Thread.currentThread().getName() + " trying to acquire lock on LOCK B");
     synchronized (lockB) {
       System.out.println(Thread.currentThread().getName() + " acquired lock on LOCK B");
+      System.out.println(Thread.currentThread().getName() + " trying to acquire lock on LOCK B");
+      synchronized (lockA) {
+        System.out.println(Thread.currentThread().getName() + " acquired lock on LOCK B");
+      }
     }
   }
 
   public static void main(String[] args) {
     Deadlock deadlock = new Deadlock();
-    Runnable usePenWithPaper =
-        new Runnable() {
-          @Override
-          public void run() {
-            //                deadlock.
+    Runnable usePenAndThenPaper =
+        () -> {
+          try {
+            deadlock.usePen();
+          } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
           }
         };
+
+    Runnable usePaperAndThenPen =
+        () -> {
+          try {
+            deadlock.usePaper();
+          } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+          }
+        };
+
+    Thread t1 = new Thread(usePenAndThenPaper, "Thread 1");
+    Thread t2 = new Thread(usePaperAndThenPen, "Thread 2");
+    t1.start();
+    t2.start();
   }
 }
